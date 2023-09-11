3ed444d81b8b38f0557f66d01f9108a8fd46e3ad
WanjohiK <kelvinnyawira2022@gmail.com>
2023-09-11 12:57:13 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-	int n, m, l;
+	int n, m, k;
 
 	for (n = 48; n < 58; n++)
 	{
 		for (m = 49; m < 58; m++)
 		{
-			for (l = 50; l < 58; l++)
+			for (k = 50; k < 58; k++)
 			{
 				if (l > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
-					putchar(l);
+					putchar(k);
 					if (n != 55 || m != 56)
 					{
 						putchar(',');
