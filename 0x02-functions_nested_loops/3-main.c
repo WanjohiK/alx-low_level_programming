e5eea690f2e79929b7a9a3dc672d616d3e6087b6
WanjohiK <kelvinnyawira2022@gmail.com>
2023-09-12 13:24:46 -0700

    3-main.c

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,4 +1,3 @@
-julien@ubuntu:~/0x02$ cat 3-main.c 
 #include "main.h"
 
 /**
@@ -8,14 +7,14 @@ julien@ubuntu:~/0x02$ cat 3-main.c
 */
 int main(void)
 {
-    int r;
+int r;
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+r = _islower('H');
+_putchar(r + '0');
+r = _islower('o');
+_putchar(r + '0');
+r = _islower(108);
+_putchar(r + '0');
+_putchar('\n');
+return (0);
 }
