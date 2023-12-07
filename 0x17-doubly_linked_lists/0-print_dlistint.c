823ac80c14c06b92d900d8c19bb00fce2cc86c25
WanjohiK <kelvinnyawira2022@gmail.com>
2023-12-08 00:10:40 +0300

    python

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,30 @@
 #include "lists.h"
 
 /**
-* print_dlistint - prints all elements the linked list
-* @h: headptr  to the nodes of the list
-*
-* Return: number of nodes
-*/
-
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int i;
+	int count;
 
-	for (i = 0; h != NULL; i++)
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
+		count++;
 		h = h->next;
 	}
-	return (i);
+
+	return (count);
 }
